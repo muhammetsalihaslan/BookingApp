d7eb2f8116c564d17868f37dcdaa15bb6a07f534
salih <muhammetsalihaslan@gmail.com>
2022-12-27 22:53:26 +0300

    navbar

--- bookingapp/src/components/navbar/Navbar.jsx
+++ bookingapp/src/components/navbar/Navbar.jsx
@@ -9,10 +9,13 @@ const Navbar = () => {
         <span className="logo">msaBooking</span>
         <div className="navItem">
           <button className="tbtn">TRY</button>
+          {/* <span className="tspn">Choose your currency</span> */}
           <button className="tbtn">ENG</button>
+          {/* <span className="tspn">Choose your language</span> */}
           <button className="tbtn">
             <FontAwesomeIcon className="que" icon={faCircleQuestion} />
           </button>
+          {/* <span className="tspn">Contact Customer Service</span> */}
           <button className="navButtonfx">List your property</button>
           <button className="navButton">Register</button>
           <button className="navButton">Sign in</button>
