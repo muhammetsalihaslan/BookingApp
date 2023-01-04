353be895bd93ce7889bce7a4fcf300498b0b4863
salih <muhammetsalihaslan@gmail.com>
2023-01-04 18:29:09 +0300

    home

--- bookingapp/src/pages/home/Home.jsx
+++ bookingapp/src/pages/home/Home.jsx
@@ -1,3 +1,6 @@
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
@@ -7,6 +10,9 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+      </div>
     </div>
   );
 };
