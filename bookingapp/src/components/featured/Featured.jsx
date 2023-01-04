89be15ca5d9746c9b37dc84f7cedb09fa63062cc
salih <muhammetsalihaslan@gmail.com>
2023-01-04 18:29:03 +0300

    home

---
+++ bookingapp/src/components/featured/Featured.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem"></div>
+    </div>
+  );
+};
+
+export default Featured;
