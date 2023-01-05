505aa2206766774c04e51757fc565ba0c0bd49e0
salih <muhammetsalihaslan@gmail.com>
2023-01-05 23:08:58 +0300

    featured

--- bookingapp/src/components/featured/Featured.jsx
+++ bookingapp/src/components/featured/Featured.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import "./featured.css";
 
 const Featured = () => {
   return (
     <div className="featured">
-      <div className="featuredItem"></div>
+      <div className="featuredItem">
+        <img
+          src="https://media.istockphoto.com/id/1327851924/tr/foto%C4%9Fraf/aerial-view-of-the-port-of-batumi-area-on-black-seas-coast-batumi-city-adjara-region.jpg?s=612x612&w=0&k=20&c=-ITDfhoNDwPOEvQme4AnMjUxBWYOe-KZKTboVLbzroo="
+          alt=""
+        />
+        <div className="featuredTitles">
+          <h1>Batumi</h1>
+          <img
+            className="flag"
+            src="https://cdn-icons-png.flaticon.com/512/5111/5111838.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://cdn.pixabay.com/photo/2018/03/15/05/35/water-3227164_960_720.jpg"
+          alt=""
+        />
+        <div className="featuredTitles">
+          <h1>Miami Beach</h1>
+          <img
+            className="flag"
+            src="https://cdn-icons-png.flaticon.com/512/555/555526.png"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
