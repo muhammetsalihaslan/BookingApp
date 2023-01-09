181ed89e15ea4e85b936eac72784eb81a35caa18
salih <muhammetsalihaslan@gmail.com>
2023-01-09 17:05:17 +0300

    properties

--- bookingapp/src/components/propertList/PropertyList.jsx
+++ bookingapp/src/components/propertList/PropertyList.jsx
@@ -7,6 +7,7 @@ const PropertyList = () => {
         <img
           src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
           alt=""
+          className="pListImg"
         />
         <div className="pListTitles">
           <h1>Hotels</h1>
@@ -17,6 +18,7 @@ const PropertyList = () => {
         <img
           src="https://r-xx.bstatic.com/xdata/images/hotel/300x240/119467716.jpeg?k=f3c2c6271ab71513e044e48dfde378fcd6bb80cb893e39b9b78b33a60c0131c9&o="
           alt=""
+          className="pListImg"
         />
         <div className="pListTitles">
           <h1>Apartments</h1>
@@ -27,6 +29,7 @@ const PropertyList = () => {
         <img
           src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/45450075.jpeg?k=d23cf8443780ac09f46f59e40393d75dbe64b06029b4959c60b81b7fdefc9be0&o="
           alt=""
+          className="pListImg"
         />
         <div className="pListTitles">
           <h1>Self-catering Accommodations</h1>
@@ -37,6 +40,7 @@ const PropertyList = () => {
         <img
           src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/45450082.jpeg?k=beb101b827a729065964523184f4db6cac42900c2415d71d516999af40beb7aa&o="
           alt=""
+          className="pListImg"
         />
         <div className="pListTitles">
           <h1>Hostels</h1>
@@ -47,6 +51,7 @@ const PropertyList = () => {
         <img
           src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/45450056.jpeg?k=251e2507d43a24a4c58bb961b8d157147d56efbf91b49f9606bc768c58f581e4&o="
           alt=""
+          className="pListImg"
         />
         <div className="pListTitles">
           <h1>B&Bs</h1>
@@ -57,6 +62,7 @@ const PropertyList = () => {
         <img
           src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/45450066.jpeg?k=4adfab312f5d26da9f81da48d8c95ca8f108215b2c84085590891a9e0e17b144&o="
           alt=""
+          className="pListImg"
         />
         <div className="pListTitles">
           <h1>Homestays</h1>
@@ -67,6 +73,7 @@ const PropertyList = () => {
         <img
           src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/45450073.jpeg?k=795a94c30433de1858ea52375e8190a962b302376be2e68aa08be345d936557d&o="
           alt=""
+          className="pListImg"
         />
         <div className="pListTitles">
           <h1>Guest houses</h1>
