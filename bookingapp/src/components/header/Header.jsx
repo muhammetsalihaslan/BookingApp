04683903f9ceb7607f545ea373bce53ceef6786a
salih <muhammetsalihaslan@gmail.com>
2023-01-11 17:39:24 +0300

    list date

--- bookingapp/src/components/header/Header.jsx
+++ bookingapp/src/components/header/Header.jsx
@@ -122,6 +122,7 @@ const Header = ({ type }) => {
                     moveRangeOnFirstSelection={false}
                     ranges={date}
                     className="date"
+                    minDate={new Date()}
                   />
                 )}
               </div>
--- bookingapp/src/pages/list/List.jsx
+++ bookingapp/src/pages/list/List.jsx
@@ -4,12 +4,14 @@ import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./list.css";
 import { format } from "date-fns";
+import { DateRange } from "react-date-range";
 
 const List = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
   const [date, setDate] = useState(location.state.date);
   const [options, setOptions] = useState(location.state.options);
+  const [openDate, setOpenDate] = useState(false);
   return (
     <div>
       <Navbar />
@@ -24,13 +26,24 @@ const List = () => {
             </div>
             <div className="lsItem">
               <label>Check-in Date:</label>
-              <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
-                date[0].endDate,
-                "MM/dd/yy"
-              )}`}</span>
+              <span onClick={() => setOpenDate(!openDate)}>
+                {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+                  date[0].endDate,
+                  "MM/dd/yy"
+                )}`}
+              </span>
             </div>
           </div>
           <div className="listResult"></div>
+        </div>
+        <div className="opendate">
+          {openDate && (
+            <DateRange
+              onChange={(item) => setDate([item.selection])}
+              minDate={new Date()}
+              ranges={date}
+            />
+          )}
         </div>
       </div>
     </div>
