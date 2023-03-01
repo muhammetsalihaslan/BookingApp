2b26d7a0a6d2d1b769f877a8916d320e982c4741
salih <muhammetsalihaslan@gmail.com>
2023-03-01 15:57:21 +0300

    help center

--- bookingapp/src/pages/helpcenter/HelpCenter.jsx
+++ bookingapp/src/pages/helpcenter/HelpCenter.jsx
@@ -3,23 +3,24 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import "./helpcenter.css";
 
 const HelpCenter = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <div className="container">
-        <h3>Help Center</h3>
-        <div>
-          <h3>Welcome to the Help Center</h3>
-          <p>
+        <h3 className="h3">Help Center</h3>
+        <div className="container2">
+          <h3 className="h32">Welcome to the Help Center</h3>
+          <p style={{ padding: "8px" }}>
             Sign in to contact Customer Service – we're available 24 hours a day
           </p>
-          <div>
+          <div className="container3">
             <div>
               <FontAwesomeIcon icon={faMessage} />
               <p>Send us a message</p>
-              <span>
+              <span style={{ fontSize: `13px` }}>
                 Contact our agents about your booking, and we'll reply as soon
                 as possible.
               </span>
@@ -27,17 +28,17 @@ const HelpCenter = () => {
             <div>
               <FontAwesomeIcon icon={faPhone} />
               <p>Call us</p>
-              <span>
+              <span style={{ fontSize: `13px` }}>
                 For anything urgent, you can call us 24/7 at a local or
                 international phone number.
               </span>
             </div>
-            <button>Sign in</button>
           </div>
-          <button>Continue without an account</button>
+          <button className="btn1">Sign in</button>
+          <button className="btn2">Continue without an account</button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
